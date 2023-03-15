e602ede8a548ba4686434e4d42f0147b1640b0b4
amolu <amolujagare@gmail.com>
2023-03-15 22:17:22 +0530

    java

---
+++ src/CircleArea.java
@@ -0,0 +1,12 @@
+public class CircleArea {
+
+    public static void main(String[] args) {
+
+        int r = 15;
+        double area;
+
+        area = 3.14 * r * r ;
+
+        System.out.println("Area="+area);
+    }
+}
---
+++ src/LessThanGreaterThan.java
@@ -0,0 +1,8 @@
+public class LessThanGreaterThan {
+
+    public static void main(String[] args) {
+        int a = 35;
+
+        System.out.println( a>35 );
+    }
+}
