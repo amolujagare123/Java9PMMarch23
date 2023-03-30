23db56d359873cc6b693a3bba2ef697d7cea22dc
amolu <amolujagare@gmail.com>
2023-03-30 22:06:14 +0530

    java

---
+++ src/DateDemo/DateDemo1.java
@@ -0,0 +1,26 @@
+package DateDemo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateDemo1 {
+
+    public static void main(String[] args) {
+        Date date  = new Date();
+        System.out.println(date);
+
+        SimpleDateFormat sd = new SimpleDateFormat("dd/M/yyyy");
+        String dateStr = sd.format(date);
+        System.out.println(dateStr);
+
+        SimpleDateFormat sd0 = new SimpleDateFormat("dd-MMMM-yyyy");
+        String dateStr0 =sd0.format(date);
+        System.out.println(dateStr0);
+
+        SimpleDateFormat sd1 = new SimpleDateFormat("dd | MMMM yy h:mm");
+        String dateStr1 = sd1.format(date);
+        System.out.println(dateStr1);
+
+
+    }
+}
